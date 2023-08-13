4bb18027111324124dc5d8bdfc2f7172520523c9
raghadp <106835325+raghadp@users.noreply.github.com>
2023-08-13 05:59:05 +0300

    Update fatch.php

--- fatch.php
+++ fatch.php
@@ -4,7 +4,7 @@
   $servername = "localhost";
   $username = "root";
   $password = "";
-  $dbname = "fiverr_raghadpink2";
+  $dbname = "raghadpink2";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
